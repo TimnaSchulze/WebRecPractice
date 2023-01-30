aeb0b9840719984ba23d54b15531613882b97c68
Timna Schulze <Timna.lansel@gmail.com>
2023-01-29 16:06:58 -0800

    BUGFIX: use setAttribute and other fixes from Amanda Guan

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,43 @@
-window.addEventListener("load", function() {
-  let form = document.querySelector("form");
+window.onload = function() {
+  let body = document.querySelector("body");
 
-  const h1Old = document.querySelector("h1");
-  h1Old.remove();
+  let h1Old = document.querySelector("h1");
+  h1Old.innerText = "Webpage Recreation Practice";
 
-  const h1New = document.createElement("h1");
-  h1New.append("Webpage Recreation Practice");
+  const p = document.createElement("p");
+  p.innerText = "The HTML of this webpage was created with JavaScript";
+  body.append(p);
 
-  const p1 = document.createElement("p");
-  p1.append("The HTML of this webpage was created with JavaScript");
-
-  const tanagerPic = document.createElement("img");
-  tanagerPic.append("img src='https://cdn.download.ams.birds.cornell.edu/api/v1/asset/202984001' alt='This is an image of a Multicolored Tanager bird from birdsoftheworld.org' style='width:50%'");
+  const img = document.createElement("img");
+  img.setAttribute(
+    "src",
+    "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/202984001"
+  );
+  img.setAttribute(
+    "alt",
+    "This is an image of a Multicolored Tanager bird from birdsoftheworld.org"
+  );
+  img.setAttribute("style", "width:50%");
+  body.append(img);
 
   const h1NewTwo = document.createElement("h1");
-  h1NewTwo.append("Facts about the Multicolored Tanager");
+  h1NewTwo.innerText = "Facts about the Multicolored Tanger";
+  body.append(h1NewTwo);
 
   const ul = document.createElement("ul");
-  ul.setAttribute("id", "Facts");
-  document.querySelector("div").append("ul");
   const liOne = document.createElement("li");
   const liTwo = document.createElement("li");
   liOne.append("It is endemic to the mountains of Colombia");
-  liTwo.append("It usually searches for insects on the underside of leaves of outer limbs while clinging to leaves with its feet.")
+  liTwo.append("It usually searches for insects on the underside of leaves of outer limbs while clinging to leaves with its feet.");
+  ul.append(liOne, liTwo);
+  body.append(ul);
 
   const h2 = document.createElement("h2");
-  h2.append("Source");
-  const sourceLink = document.createElement("a");
-  sourceLink.append("a href='https://en.wikipedia.org/wiki/Multicoloured_tanager'>Wikipedia");
-});
+  h2.innerText = "Source";
+  body.append(h2);
+
+  const a = document.createElement("a");
+  a.setAttribute("href", "'https://en.wikipedia.org/wiki/Multicoloured_tanager'>Wikipedia");
+  a.append("Wikipedia");
+  body.append(a)
+};
